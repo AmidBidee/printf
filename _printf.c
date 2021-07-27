29261e28ca76b7d2ec27331dfe836995f07e254f
maxwell150 <maxwelgithumbi@gmail.com>
2021-07-27 21:13:59 +0000

    update

--- _printf.c
+++ _printf.c
@@ -6,55 +6,70 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list vars;
-	int i = 0, j = 0, index;
-	char buffer[200] = {0};
-	/* char tmp[20]; */
-	char *str_arg;
+	char *traverse;
+	unsigned int i;
+	char *s;
+	va_list arg;
+	va_start(arg, format);
 
-	va_start(vars, format);
-	while (format && format[i])
+	for (traverse = format; *traverse != '\0'; traverse++)
 	{
-		if (format[i] == '%')
+		while (*traverse != '%')
 		{
-			i++;
-			switch (format[i])
-			{
-				case 'c':
-					buffer[j] = (char)va_arg(vars, int);
-					j++;
-					break;
-				case 's':
-					str_arg = va_arg(vars, char*);
-					strcpy(&buffer[j], str_arg);
-					j += strlen(str_arg);
-					break;
-				case '%':
-					buffer[j] = format[i];
-                                        j++;
-					break;
-				default:
-					--i;
-					buffer[j] = format[i];
-					j++;
-					break;
-
-
-			}
+			_putchar (*traverse);
+			traverse++;
 		}
-		else
+		traverse++;
+		switch (*traverse)
 		{
-			buffer[j] = format[i];
-			j++;
+			case 'c':
+				i = va_arg(arg, int);
+				_putchar(i);
+				break;
+			case 'd':
+				i = va_arg(arg, int);
+				if (i < 0)
+				{
+					i = -i;
+					_putchar('-');
+				}
+				puts(convert(i, 10));
+				break;
+			case 'o':
+				i = va_arg(arg, unsigned int);
+				puts(convert(i, 8));
+				break;
+			case 's':
+				s = va_arg(arg, char *);
+				puts(s);
+				break;
+			case 'x':
+				i = va_arg(arg, unsigned int);
+				puts(convert(i, 16));
+				break;
 		}
-		i++;
 	}
-
-	for (index = 0; index < j; index++)
-	{
-		_putchar(buffer[index]);
-	}
-	va_end(vars);
-	return (j);
+	va_end(arg);
 }
+/**
+* convert - does the necessary conversions
+* @num: parameter for converion
+* @base:  parameter for conversion
+* Return: returns a pointer
+*/
+char *convert(unsigned int num, int base)
+{
+	static char Representation[] = "0123456789ABCDEF";
+	static char buffer[50];
+	char *ptr;
 
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do {
+		*--ptr = Representation[num % base];
+		num /= base;
+	} while (num != 0);
+
+	return (ptr);
+}
